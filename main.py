7cb6eb202f1b98752e5962ee57f1e7d9e11648d0
Liam Douglass <liamadouglass@gmail.com>
2019-02-19 16:47:20 -0500

    added global vars

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ import store_data
 import datetime
 from multiprocessing import Pool
 import pysize
+
+initial_url = ""
+wiki_map = {}
+num_repeats = 0
+num_pages = 0
+errors = []
+frontier = []
+keys = []
