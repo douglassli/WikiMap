584648d4d595fe6e58c0e22bddc54a97158672cb
Liam Douglass <liamadouglass@gmail.com>
2019-02-19 16:48:29 -0500

    implemented the parse_page function

--- main.py
+++ main.py
@@ -16,3 +16,34 @@ num_pages = 0
 errors = []
 frontier = []
 keys = []
+
+def parse_page(tpl):
+    raw_html, search_url, title, get_time, time_of_get = tpl
+
+    parse_start = time.time()
+    root = html.fromstring(raw_html)
+    parse_time = time.time() - parse_start
+
+    analysis_start = time.time()
+
+    out_links = []
+    out_titles = []
+    a_tags = root.cssselect("a[title][href^='/wiki/']")
+
+    for a in a_tags:
+        temp_url = a.get("href")
+        temp_title = a.get("title")
+
+        if temp_title == "Help:Category":
+            break
+
+        if temp_title not in out_titles and not bool(re.search("([*\S]:[*\S])", temp_title + temp_url)):
+            out_links.append("https://en.wikipedia.org" + temp_url)
+            out_titles.append(temp_title)
+
+    analysis_time = time.time() - analysis_start
+    total_time = get_time + parse_time + analysis_time
+
+    page = (title, search_url, out_titles, out_links, total_time, get_time, parse_time, analysis_time, time_of_get)
+
+    return page
