e1c4f1677c92fdd789233cd6d024e509e9d52733
Liam Douglass <liamadouglass@gmail.com>
2019-02-19 16:45:46 -0500

    added import statements

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding=utf-8 -*-
+from requests import Session
+import time
+import sys
+from lxml import html
+import re
+import store_data
+import datetime
+from multiprocessing import Pool
+import pysize
