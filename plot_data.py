d9ba01748252b4b626bb6842f7ae9f840d1873f2
Liam Douglass <liamadouglass@gmail.com>
2019-02-22 17:03:18 -0500

    midway through implementing method to get average get time per second

--- plot_data.py
+++ plot_data.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import store_data
+import math
 
 
 def seconds_from_date(date_string):
@@ -8,6 +9,18 @@ def seconds_from_date(date_string):
     seconds = float(date_string[17:])
     total_secs = (3600 * hours) + (60 * minutes) + seconds
     return total_secs
+
+
+def get_average_per_second(get_list, time_list):
+    gets_dict = {}
+    for i, time in enumerate(time_list):
+        if math.floor(time) not in gets_dict:
+            gets_dict[math.floor(time)] = []
+        gets_dict[math.floor(time)].append(get_list[i])
+
+    avg_dict = {}
+    for key in gets_dict.keys():
+        a
 
 
 def plot_get_vs_time(file_name):
