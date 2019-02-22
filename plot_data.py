004fcab6ae7029df4ba0c95fa48fe0a6c79bb3c2
Liam Douglass <liamadouglass@gmail.com>
2019-02-22 16:42:46 -0500

    added function to retrieve the seconds from a time string

--- plot_data.py
+++ plot_data.py
@@ -1,2 +1,9 @@
 import matplotlib.pyplot as plt
 
+
+def seconds_from_date(date_string):
+    hours = float(date_string[11:13])
+    minutes = float(date_string[14:16])
+    seconds = float(date_string[17:])
+    total_secs = (3600 * hours) + (60 * minutes) + seconds
+    return total_secs
