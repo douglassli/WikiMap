6ce7137f7ca7bd157533da6d901133b06ee45c59
Liam Douglass <liamadouglass@gmail.com>
2019-02-22 17:10:58 -0500

    changed name of function to reflect change

--- plot_data.py
+++ plot_data.py
@@ -10,7 +10,7 @@ def seconds_from_date(date_string):
     return total_secs
 
 
-def get_average_per_second(get_list, time_list):
+def get_average_per_ten_sec(get_list, time_list):
     gets_dict = {}
     for i, time in enumerate(time_list):
         if round(time, -1) not in gets_dict:
@@ -28,7 +28,7 @@ def plot_get_vs_time(file_name):
     df = store_data.read_partial(file_name, ["Get Time", "Time of Expansion"])
     get_list = list(df["Get Time"])
     time_list = [seconds_from_date(i) for i in list(df["Time of Expansion"])]
-    avg_dict = get_average_per_second(get_list, time_list)
+    avg_dict = get_average_per_ten_sec(get_list, time_list)
     plt.plot(avg_dict.keys(), avg_dict.values())
     plt.ylabel("Get Time (sec)")
     plt.xlabel("Time of Get (sec)")
