6fabac3a9b10bbfb87905483bea021d57cba5b42
Liam Douglass <liamadouglass@gmail.com>
2019-02-22 16:34:35 -0500

    changed testing parameters

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
     url_small = "https://en.wikipedia.org/wiki/Contract_manufacturer"  # 50 out-links Depth 1: ~3 sec Depth 2: ~8.5 min
 
     start = time.time()
-    map_wiki(2, url_med)
+    map_wiki(2, url_small)
     end = time.time()
 
     print_errors()
