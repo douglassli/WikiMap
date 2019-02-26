bdf5579c93370dd6ff45dd8ae220d5bc62845bb3
Liam Douglass <liamadouglass@gmail.com>
2019-02-26 14:40:32 -0500

    updated testing parameters

--- main.py
+++ main.py
@@ -199,14 +199,14 @@ def parse_page(tpl):
 
 
 if __name__ == "__main__":
-    sys.stdout = open("stdout.txt", mode='w')
+    # sys.stdout = open("stdout.txt", mode='w')
     store_data.initialize_csv("output.csv")
 
     url_med = "https://en.wikipedia.org/wiki/IPhone"               # 855 out-links Depth 1: ~45 sec Depth 2: ~1 hr
     url_small = "https://en.wikipedia.org/wiki/Contract_manufacturer"  # 50 out-links Depth 1: ~3 sec Depth 2: ~8.5 min
 
     start = time.time()
-    map_wiki(3, url_small)
+    map_wiki(1, url_small)
     end = time.time()
 
     print_errors()
