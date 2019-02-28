80f9ee2f426b50e7e031a41ab743f1b09fd05df3
Liam Douglass <liamadouglass@gmail.com>
2019-02-28 14:47:23 -0500

    modified get edges function to store them in csv as it goes to reduce memory usage

--- plot_network.py
+++ plot_network.py
@@ -25,13 +25,15 @@ def get_nodes(file_name):
     return titles
 
 
-def get_edges(file_name):
+def prepare_edges(file_name, output_file):
     num_read = 0
-    edges = set()
+    store_data.initialize_csv(file_name)
 
     while True:
         df = store_data.read_partial_section(file_name, ["Page Title", "Out-link Titles"], 1000, num_read)
         tpls = list(df.itertuples(index=False, name=None))
+
+        edges = []
 
         if len(tpls) == 0:
             break
@@ -41,18 +43,13 @@ def get_edges(file_name):
         for page in tpls:
             temp_title = page[0]
             for target in ast.literal_eval(page[1]):
-                if (temp_title, target) not in edges and (target, temp_title) not in edges:
-                    edges.add((temp_title, target))
+                edges.append((temp_title, target))
+
+        df = pandas.DataFrame(edges)
+        df.to_csv(output_file, index=False, mode="a", header=["Source", "Target"])
 
     return list(edges)
 
 
-def store_edge_data_csv(input_file, output_file):
-    edges = get_edges(input_file)
-    store_data.initialize_csv(output_file)
-    df = pandas.DataFrame(edges)
-    df.to_csv(output_file, index=False, mode="a", header=["Source", "Target"])
-
-
 if __name__ == '__main__':
-    store_edge_data_csv("output.csv", "edges.csv")
+    prepare_edges("output.csv", "edges.csv")
