68f300ca6c0be06c7cfac73027cd47b8a2d132ea
Liam Douglass <liamadouglass@gmail.com>
2019-02-28 16:22:42 -0500

    added function that just gets the edges of major nodes

--- plot_network.py
+++ plot_network.py
@@ -3,7 +3,7 @@ import ast
 import pandas
 
 
-def get_nodes_attributes(file_name, output_file):
+def get_major_nodes_attributes(file_name, output_file):
     num_read = 0
     store_data.initialize_csv(output_file)
 
@@ -21,7 +21,7 @@ def get_nodes_attributes(file_name, output_file):
         df.to_csv(output_file, index=False, mode="a", header=["Node", "Num Out-links"])
 
 
-def prepare_edges(file_name, output_file):
+def prepare_all_edges(file_name, output_file):
     num_read = 0
     store_data.initialize_csv(output_file)
 
@@ -45,6 +45,32 @@ def prepare_edges(file_name, output_file):
         df.to_csv(output_file, index=False, mode="a", header=["Source", "Target"])
 
 
+def prepare_major_edges(file_name, output_file):
+    num_read = 0
+    store_data.initialize_csv(output_file)
+    major_nodes = set(store_data.read_partial(file_name, ["Page Title"])["Page Title"])
+
+    while True:
+        df = store_data.read_partial_section(file_name, ["Page Title", "Out-link Titles"], 1000, num_read)
+        tpls = list(df.itertuples(index=False, name=None))
+
+        edges = []
+
+        if len(tpls) == 0:
+            break
+
+        num_read += len(tpls)
+
+        for page in tpls:
+            temp_title = page[0]
+            for target in ast.literal_eval(page[1]):
+                if temp_title in major_nodes and target in major_nodes:
+                    edges.append((temp_title, target))
+
+        df = pandas.DataFrame(edges)
+        df.to_csv(output_file, index=False, mode="a", header=["Source", "Target"])
+
+
 if __name__ == '__main__':
-    get_nodes_attributes("output.csv", "nodes.csv")
-    prepare_edges("output.csv", "edges.csv")
+    get_major_nodes_attributes("output.csv", "nodes.csv")
+    prepare_all_edges("output.csv", "edges.csv")
