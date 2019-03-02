b3b9cc6f30ac9f2f1db0244cf4cbb461dbb42bc9
Liam Douglass <liamadouglass@gmail.com>
2019-03-02 17:09:10 -0500

    added data definition and function definitions to header file

--- wiki_search/map_vec.h
+++ wiki_search/map_vec.h
@@ -1,6 +1,16 @@
+#include node.h
+
 #ifndef MAP_VEC_H
 #define MAP_VEC_H
 
+typedef struct map_vec {
+	node* nodes;
+	long size;
+	long cap;
+} map_vec;
 
+map_vec* make_map();
+void free_map(map_vec* map);
+void push_map(map_vec* map, node* node);
 
 #endif
