9fde05f5a20cf2e585029419cf120d5897b3eb69
Liam Douglass <liamadouglass@gmail.com>
2019-03-02 16:48:32 -0500

    added header file for a node data structure that holds info for a node and its successors

---
+++ wiki_search/node.h
@@ -0,0 +1,16 @@
+#ifndef NODE_H
+#define NODE_H
+
+typedef struct node {
+	long node;
+	long size;
+	long cap;
+	long* data;
+} node;
+
+node* make_node(long node_val);
+void node_push(node* xs, long succ);
+void free_node(node* xs);
+void print_node(node* xs);
+
+#endif
