1a755b0f23b459befbcb3333fa8de06e191efd93
Liam Douglass <liamadouglass@gmail.com>
2019-03-02 16:48:54 -0500

    added implementation of header file node.h

---
+++ wiki_search/node.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "node.h"
+/*
+	typedef struct node {
+		long node;
+		long size;
+		long cap;
+		long* data;
+	} node;
+*/
+
+
+node* make_node(long node_val) {
+	node* xs = malloc(sizeof(node));
+	xs->node = node_val;
+	xs->data = malloc(2 * sizeof(long));
+	xs->size = 0;
+	xs->cap = 2;
+	return xs;
+}
+
+void push_node(node* xs, long succ) {
+	if (xs->size >= xs->cap) {
+		xs->cap *= 2;
+		xs->data = realloc(xs->data, xs->cap * sizeof(long));
+	}
+
+	xs->data[xs->size] = succ;
+	xs->size += 1;
+}
+
+void free_node(node* xs) {
+	free(xs->data);
+	free(xs);
+}
+
+void print_node(node* xs) {
+	
+}
