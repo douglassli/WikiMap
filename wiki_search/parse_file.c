eb6cad3f6b4fce1b85f0d80cc29ea296d2dbd5d7
Liam Douglass <liamadouglass@gmail.com>
2019-03-02 21:24:39 -0500

    added import headers to parse file

--- wiki_search/parse_file.c
+++ wiki_search/parse_file.c
@@ -1,7 +1,7 @@
-// This file will parse an input text file with the first number in each line being
-// the number representing a node and the following numbers in the line representing 
-// the successors of that given node.
+#include <stdlib.h>
+#include <stdio.h>
 
-int main() {
-	return 0;
-}
+#include "map_vec.h"
+#include "node.h"
+
+
