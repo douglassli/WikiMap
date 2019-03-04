96b0a0025a013018722f17e638ade526f4f4965e
Liam Douglass <liamadouglass@gmail.com>
2019-03-04 08:19:26 -0700

    code formatting

--- wiki_search/map_vec.c
+++ wiki_search/map_vec.c
@@ -5,41 +5,41 @@
 #include "node.h"
 
 /*
-	typedef struct map_vec {
-		node* nodes;
-		long size;
-		long cap;
-	} map_vec;
+    typedef struct map_vec {
+        node* nodes;
+        long size;
+        long cap;
+    } map_vec;
 */
 
 map_vec* make_map() {
-	map_vec* map = malloc(sizeof(map_vec));
-	map->nodes = malloc(2 * sizeof(node));
-	xs->size = 0;
-	xs->cap = 2;
-	return map;
+    map_vec* map = malloc(sizeof(map_vec));
+    map->nodes = malloc(2 * sizeof(node));
+    xs->size = 0;
+    xs->cap = 2;
+    return map;
 }
 
 void free_map(map_vec* map) {
-	for (long i = 0; i < map->size; ++i) {
-		free_node(map->nodes[i]);
-	}
-	free(map->nodes);
-	free(map);
+    for (long i = 0; i < map->size; ++i) {
+        free_node(map->nodes[i]);
+    }
+    free(map->nodes);
+    free(map);
 }
 
 void push_map(map_vec* map, node* node) {
-	if (map->size >= map->cap) {
-		map->cap *= 2;
-		map->nodes = realloc(map->nodes, map->cap * sizeof(node));
-	}
+    if (map->size >= map->cap) {
+        map->cap *= 2;
+        map->nodes = realloc(map->nodes, map->cap * sizeof(node));
+    }
 
-	map->nodes[map->size] = node;
-	map->size += 1;
+    map->nodes[map->size] = node;
+    map->size += 1;
 }
 
 void print_map(map_vec* map) {
-	for (long i = 0; i < map->size; ++i) {
-		print_node(map->nodes[i]);
-	}
+    for (long i = 0; i < map->size; ++i) {
+        print_node(map->nodes[i]);
+    }
 }
