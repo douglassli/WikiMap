8d53747130ce4380508617c71aac45abd9ace04e
Liam Douglass <liamadouglass@gmail.com>
2019-03-04 08:36:49 -0700

    added functionality to read file line by line

--- wiki_search/parse_file.c
+++ wiki_search/parse_file.c
@@ -6,7 +6,23 @@
 
 
 map_vec* parse_map_file(char* file) {
-	
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    while (1) {
+        linelen = getline(&line, &linecap, fp);
+        if (line[0] == "*") {
+            break;
+        }
+        fwrite(line, linelen, 1, stdout);
+    }
+    return make_map();
 }
 
+
+int main() {
+    char* file = "map.txt";
+    parse_map_file(file);
+    return 0;
+}
 
