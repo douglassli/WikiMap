b88ed4c29533c4bcccb216ae398d2699cc47679b
Liam Douglass <liamadouglass@gmail.com>
2019-03-07 10:32:01 -0700

    began implementation to work through output file

--- resume_scrape.py
+++ resume_scrape.py
@@ -2,5 +2,16 @@ import pandas
 import store_data
 import ast
 
+
 def get_num_read_frontier(file_name):
-    
+    num_read = 0
+    num_read_frontier = 0
+
+    while True:
+        df = store_data.read_partial_section(file_name, ["Depth"], 1000, num_read)
+        tpls = list(df.itertuples(index=False, name=None))
+
+        if len(tpls) == 0:
+            break
+
+        num_read += len(tpls)
