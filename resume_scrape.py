7577c21387bf869f7bf1c4f129ec84075523bb1d
Liam Douglass <liamadouglass@gmail.com>
2019-03-07 10:37:04 -0700

    implemented function to find the max depthe of a file

--- resume_scrape.py
+++ resume_scrape.py
@@ -3,6 +3,26 @@ import store_data
 import ast
 
 
+def find_max_depth(file_name):
+    num_read = 0
+    max_depth = 0
+
+    while True:
+        df = store_data.read_partial_section(file_name, ["Depth"], 1000, num_read)
+        depths = list(df["Depth"])
+
+        if len(depths) == 0:
+            break
+
+        num_read += len(depths)
+
+        max_d = max(depths)
+        if max_d > max_depth:
+            max_depth = max_d
+
+    return max_depth
+
+
 def get_num_read_frontier(file_name):
     num_read = 0
     num_read_frontier = 0
