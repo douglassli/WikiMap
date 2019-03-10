8f48af835bf3c032050f2d8418caf36eeccb1ed2
Liam Douglass <liamadouglass@gmail.com>
2019-03-10 10:38:25 -0600

    implemented function to get a given node from the given map

--- wiki_search/search.c
+++ wiki_search/search.c
@@ -5,6 +5,11 @@
 #include "map_vec.h"
 #include "node.h"
 
+node* get_node(map_vec* map, long node_val) {
+    return map->nodes[node_val];
+}
+
+
 int bfs(map_vec* map, long start, long goal) {
     
 
