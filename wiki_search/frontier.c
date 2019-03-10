4412b616cababde49e9df1ee3641796390595614
Liam Douglass <liamadouglass@gmail.com>
2019-03-10 11:05:24 -0600

    intiial commit

---
+++ wiki_search/frontier.c
@@ -0,0 +1,17 @@
+#include <stdlib.c>
+#include <stdio.h>
+
+#include "frontier.h"
+
+/*
+    typedef struct frontier {
+        long size;
+        long cap;
+        long* vals;
+    } frontier;
+*/
+
+frontier* make_frontier() {
+    frontier* fr = malloc(sizeof(frontier));
+
+}
