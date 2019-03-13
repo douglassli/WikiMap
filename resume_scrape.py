de48366645f5c4ead8eefd48757ba25d32075039
Liam Douglass <liamadouglass@gmail.com>
2019-03-13 02:24:46 -0400

    changed number of items to load at a time for efficiency

--- resume_scrape.py
+++ resume_scrape.py
@@ -1,6 +1,7 @@
 import pandas
 import store_data
 import ast
+import time
 
 
 def get_keys(file_name):
@@ -8,7 +9,7 @@ def get_keys(file_name):
     keys_set = set()
 
     while True:
-        df = store_data.read_partial_section(file_name, ["Page Title"], 1000, num_read)
+        df = store_data.read_partial_section(file_name, ["Page Title"], 1000000, num_read)
         titles = list(df["Page Title"])
 
         if len(titles) == 0:
@@ -27,7 +28,7 @@ def find_max_depth(file_name):
     max_depth = 0
 
     while True:
-        df = store_data.read_partial_section(file_name, ["Depth"], 1000, num_read)
+        df = store_data.read_partial_section(file_name, ["Depth"], 1000000, num_read)
         depths = list(df["Depth"])
 
         if len(depths) == 0:
@@ -49,7 +50,7 @@ def get_num_read_frontier(file_name):
     max_depth = find_max_depth(file_name)
 
     while True:
-        df = store_data.read_partial_section(file_name, ["Depth"], 1000, num_read)
+        df = store_data.read_partial_section(file_name, ["Depth"], 1000000, num_read)
         depths = list(df["Depth"])
 
         if len(depths) == 0:
@@ -62,3 +63,10 @@ def get_num_read_frontier(file_name):
                 num_read_frontier += 1
 
     return num_read_frontier
+
+
+if __name__ == '__main__':
+    start = time.time()
+    get_keys("Data Sets/Small3 Test 1/small3_full_output.csv")
+    total = time.time() - start
+    print("Time: {0:.1f}".format(total))
