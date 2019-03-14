448642ce1c22a6e6e254a79f7f3e40ba6c6f7531
Liam Douglass <liamadouglass@gmail.com>
2019-03-13 21:44:54 -0400

    finished implementing bfs search algorithm

--- wiki_search/search.c
+++ wiki_search/search.c
@@ -14,6 +14,7 @@ node* get_node(map_vec* map, long node_val) {
 
 int bfs(map_vec* map, long start, long goal) {
     node* initial_node = make_node(start);
+    push_node(initial_node, start);
     frontier* fr = make_frontier();
     push_frontier(fr, (long*)initial_node);
     explored* ex = make_explored();
@@ -24,6 +25,11 @@ int bfs(map_vec* map, long start, long goal) {
         }
 
         node* curr_node = (node*)pop_first_frontier(fr);
+
+        if (contains_explored(ex, curr_node->node)) {
+            continue;
+        }
+
         explored_push(ex, curr_node->node);
 
         if (curr_node->node == goal) {
@@ -32,7 +38,17 @@ int bfs(map_vec* map, long start, long goal) {
             break;
         }
 
+        node* succ_nodes = get_node(map, curr_node->node);
 
+        for (long ii  = 0; ii < succ_nodes->size; ++ii) {
+            long succ = succ_nodes->data[ii];
+            if (!contains_explored(ex, succ)) {
+                node* temp_node = copy_node(curr_node);
+                temp_node->node = succ;
+                push_node(temp_node, succ);
+                push_frontier(fr, (long*)temp_node);
+            }
+        }
     }
 
     return 0;
