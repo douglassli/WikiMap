0cadb681964fbd2d770eb6e8b6710c586af99e65
Liam Douglass <liamadouglass@gmail.com>
2019-03-13 20:41:19 -0400

    added additional functions for explored header file

--- wiki_search/explored_vec.h
+++ wiki_search/explored_vec.h
@@ -8,6 +8,9 @@ typedef struct explored {
 } explored;
 
 explored* make_explored();
-
+void push_explored(explored* ex, long val);
+void free_explored(explored* ex);
+int contains_explored(explored* ex, long val);
+void print_explored(explored* ex);
 
 #endif
