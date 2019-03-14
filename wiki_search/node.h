044ebb4895a0ee90b32132c4810c0b9727b18375
Liam Douglass <liamadouglass@gmail.com>
2019-03-13 21:34:31 -0400

    added copy node function

--- wiki_search/node.h
+++ wiki_search/node.h
@@ -9,6 +9,7 @@ typedef struct node {
 } node;
 
 node* make_node(long node_val);
+node* copy_node(node* xs);
 void push_node(node* xs, long succ);
 void free_node(node* xs);
 void print_node(node* xs);
