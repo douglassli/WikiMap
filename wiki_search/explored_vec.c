84e8184b49d5e1665e3651423fb1f3b972f2cb3d
Liam Douglass <liamadouglass@gmail.com>
2019-03-13 20:46:02 -0400

    implemented make explored function

--- wiki_search/explored_vec.c
+++ wiki_search/explored_vec.c
@@ -2,3 +2,11 @@
 #include <stdio.h>
 
 #include "explored_vec.h"
+
+explored* make_explored() {
+    explored* ex = malloc(sizeof(explored));
+    ex->cap = 2;
+    ex->size = 0;
+    ex->data = malloc(ex->cap * sizeof(long));
+    return ex;
+}
