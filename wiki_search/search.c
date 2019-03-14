660f3522df1afd94b2b489e9561b7874308ab462
Liam Douglass <liamadouglass@gmail.com>
2019-03-13 21:20:07 -0400

    continued implementation of bfs

--- wiki_search/search.c
+++ wiki_search/search.c
@@ -4,6 +4,8 @@
 #include "search.h"
 #include "map_vec.h"
 #include "node.h"
+#include "frontier.h"
+#include "explored_vec.h"
 
 node* get_node(map_vec* map, long node_val) {
     return map->nodes[node_val];
@@ -14,6 +16,15 @@ int bfs(map_vec* map, long start, long goal) {
     node* initial_node = make_node(start);
     frontier* fr = make_frontier();
     push_frontier(fr, (long*)initial_node);
+    explored* ex = make_explored();
+
+    while (1) {
+        if (fr->size <= 0) {
+            return -1;
+        }
+
+        node* curr_node = get_node(map, pop_first_frontier(fr));
+    }
 
     return 0;
 }
