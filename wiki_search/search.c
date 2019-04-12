7f9edd070da6886a66c22a98db6223d2c1584142
Liam Douglass <liamadouglass@gmail.com>
2019-04-12 17:35:25 -0400

    implemented main function to kick of search functions

--- wiki_search/search.c
+++ wiki_search/search.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 
 #include "bfs.h"
+#include "parse_file.h"
 
-int main() {
-    
+int main(int argc char* argv[]) {
+    if (strcmp(argv[1], "bfs") && argc == 5) {
+        printf("PARSING...\n");
+        map_vec* map = parse_map_file(argv[2]);
+        printf("SEARCHING...");
+        bfs(map, atol(argv[3]), atol(argv[4]));
+        return 0;
+    }
+
+    printf("INVALID ARGUMENTS\n");
+    return 1;
 }
