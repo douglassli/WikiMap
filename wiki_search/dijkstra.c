f5c6978dbd8853e268b1bf2005fd30f02e31032e
Liam Douglass <liamadouglass@gmail.com>
2019-04-12 18:20:48 -0400

    began implementation of dijkstra

--- wiki_search/dijkstra.c
+++ wiki_search/dijkstra.c
@@ -7,6 +7,14 @@
 #include "frontier.h"
 #include "explored_vec.h"
 
+node* get_node(map_vec* map, long node_val) {
+    return &map->nodes[node_val];
+}
+
 int dijkstra(map_vec* map, long source) {
-    
+    explored* layers = make_explored();
+    for (long i = 0; i < map->size; i++) {
+        push_explored(layers, -1);
+    }
+    layers->data[source] = 0;
 }
