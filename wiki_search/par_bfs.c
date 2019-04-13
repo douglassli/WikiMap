0b0437274eb06372ea414e985606fd7a827de3ca
Liam Douglass <liamadouglass@gmail.com>
2019-04-12 21:39:03 -0400

    began implementation of par_bfs

--- wiki_search/par_bfs.c
+++ wiki_search/par_bfs.c
@@ -8,3 +8,17 @@
 #include "explored_vec.h"
 #include "node.h"
 
+map_vec* global_map;
+frontier* global_fr;
+explored* global_dists;
+
+int par_bfs(map_vec* map, long source) {
+    global_map = map;
+    global_fr = make_frontier();
+    global_dists = make_explored();
+    for (long i = 0; i < global_map->size; i++) {
+        push_explored(global_dists, -1);
+    }
+
+    return 0;
+}
