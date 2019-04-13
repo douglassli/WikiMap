b95a6268544bf6a8ea352ea4ad0465f57c060efa
Liam Douglass <liamadouglass@gmail.com>
2019-04-12 20:40:12 -0400

    added function to retrieve node from map

--- wiki_search/map_vec.h
+++ wiki_search/map_vec.h
@@ -13,5 +13,6 @@ map_vec* make_map();
 void free_map(map_vec* map);
 void push_map(map_vec* map, node* node);
 void print_map(map_vec* map);
+node* map_get_node(map_vec* map, long node_val);
 
 #endif
