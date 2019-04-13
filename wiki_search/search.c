d68bdcc7fac413ccf77b6648b57deb7bdd654de2
Liam Douglass <liamadouglass@gmail.com>
2019-04-13 19:28:42 -0400

    added more detailed print statements

--- wiki_search/search.c
+++ wiki_search/search.c
@@ -14,19 +14,29 @@ int main(int argc, char* argv[]) {
         printf(" - Goal:       %ld\n\n", atol(argv[4]));
         printf("Parsing input file...\n");
         map_vec* map = parse_map_file(argv[2]);
+        printf("Finished parsing.\n\n");
         printf("Starting search...\n");
         bfs(map, atol(argv[3]), atol(argv[4]));
         return 0;
     } else if (strcmp(argv[1], "dijkstra") == 0 && argc == 4) {
-        printf("PARSING...\n");
+        printf("Running Dijkstra's Algorithm\n");
+        printf(" - Input File: %s\n", argv[2]);
+        printf(" - Source:     %ld\n\n", atol(argv[3]));
+        printf("Parsing input file...\n");
         map_vec* map = parse_map_file(argv[2]);
-        printf("SEARCHING...\n");
+        printf("Finished parsing.\n\n");
+        printf("Starting search...\n");
         dijkstra(map, atol(argv[3]));
         return 0;
     } else if (strcmp(argv[1], "pbfs") == 0 && argc == 5) {
-        printf("PARSING...\n");
+        printf("Running Parallel Breadth First Search\n");
+        printf(" - Input file:  %s\n", argv[2]);
+        printf(" - Source:      %ld\n", atol(argv[3]));
+        printf(" - Num Threads: %d\n\n", atoi(argv[4]));
+        printf("Parsing input file...\n");
         map_vec* map = parse_map_file(argv[2]);
-        printf("SEARCHING...\n");
+        printf("Finished parsing.\n\n");
+        printf("Starting search...\n");
         par_bfs(map, atol(argv[3]), atoi(argv[4]));
         return 0;
     }
