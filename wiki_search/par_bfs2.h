db1f9690cb308b233f7eff7a6f09d79939b58e29
Liam Douglass <liamadouglass@gmail.com>
2019-04-18 16:54:49 -0400

    finished implementing par bfs version 2 header file

--- wiki_search/par_bfs2.h
+++ wiki_search/par_bfs2.h
@@ -1,4 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "map_vec.h"
+
+#ifndef PAR_BFS2_H
+#define PAR_BFS2_H
+
+int par_bfs2(map_vec* map, long source, int num_threads, int print_output);
+
+#endif
