ea18595b42c42845b07e89cf1893ea31cc38236c
Liam Douglass <liamadouglass@gmail.com>
2019-04-19 18:29:53 -0400

    added function headers for each of the declared functions

--- wiki_search/linked_frnts.c
+++ wiki_search/linked_frnts.c
@@ -4,4 +4,14 @@
 #include "frontier.h"
 #include "linked_frnts.h"
 
+linked_frnts* make_lfrnt() {
+    
+}
 
+void add_frnt(linked_frnts* lfrnt, frontier* frnt) {
+
+}
+
+void free_lfrnt(linked_frnts* lfrnt) {
+    
+}
