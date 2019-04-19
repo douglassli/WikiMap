10ab832c1d55ec491a3e9e04b93e5550a26bdeac
Liam Douglass <liamadouglass@gmail.com>
2019-04-19 18:12:20 -0400

    changed linked array list structure to contain a frontier for ease of forming linked list

---
+++ wiki_search/linked_frnts.h
@@ -0,0 +1,13 @@
+#include "frontier.h"
+
+#ifndef LINKED_FRONTIERS_H
+#define LINKED_FRONTIERS_H
+
+typedef struct flink {
+    frontier* frnt;
+    struct alink array* next;
+} flink;
+
+typedef struct 
+
+#endif
