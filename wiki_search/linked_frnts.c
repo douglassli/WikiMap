7b013b7e5ab3c7831b5ad2b8a40fef679a70dc91
Liam Douglass <liamadouglass@gmail.com>
2019-04-19 18:36:40 -0400

    implemented create linked frnts

--- wiki_search/linked_frnts.c
+++ wiki_search/linked_frnts.c
@@ -5,11 +5,16 @@
 #include "linked_frnts.h"
 
 linked_frnts* make_lfrnts() {
-    
+    linked_frnts* new_lfrnt = malloc(sizeof(linked_frnts));
+    new_lfrnt->tot_size = 0;
+    new_lfrnt->head = 0;
+    new_lfrnt->tail = 0;
+    return new_lfrnt;    
 }
 
 void add_frnt(linked_frnts* lfrnt, frontier* frnt) {
-
+    flink* new_flink = malloc(sizeof(flink));
+    new_flink->lfr = frnt;
 }
 
 void free_lfrnts(linked_frnts* lfrnt) {
