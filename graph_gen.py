e2c41b94fd92b821305e8c287fb07fe95cf80190
Liam Douglass <liamadouglass@gmail.com>
2019-04-21 17:34:54 -0400

    added main statement and informational print statements

--- graph_gen.py
+++ graph_gen.py
@@ -10,8 +10,15 @@ def gen_graph(num_nodes, branch_low, branch_high):
 
         num_branches = random.randint(branch_low, branch_high)
         num_edges += num_branches
+
         for branch_num in range(num_branches):
             next_edge = random.randint(0, num_nodes)
-            search_file.write(str(branch_num) + " ")
+            search_file.write(str(next_edge) + " ")
 
         search_file.write("\n")
+
+    print("Made graph with {} nodes and {} edges".format(num_nodes, num_edges))
+
+
+if __name__ == '__main__':
+    gen_graph(2000000, 5, 20)
