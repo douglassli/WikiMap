134f9bf81d265edb180a60d207d3de7e3e956f00
Liam Douglass <liamadouglass@gmail.com>
2019-04-21 17:32:31 -0400

    finished implementing graph generator function

--- graph_gen.py
+++ graph_gen.py
@@ -1,2 +1,17 @@
+import random
+
+
 def gen_graph(num_nodes, branch_low, branch_high):
-    return
+    num_edges = 0
+    search_file = open("search_file.txt", "w")
+
+    for node_num in range(num_nodes):
+        search_file.write(str(node_num) + " ")
+
+        num_branches = random.randint(branch_low, branch_high)
+        num_edges += num_branches
+        for branch_num in range(num_branches):
+            next_edge = random.randint(0, num_nodes)
+            search_file.write(str(branch_num) + " ")
+
+        search_file.write("\n")
