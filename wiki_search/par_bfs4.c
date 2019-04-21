7bd283b98387ae19005877ed137dcb04b4f2a14a
Liam Douglass <liamadouglass@gmail.com>
2019-04-21 15:21:24 -0400

    renamed variables to avoid redefining variables and functions

--- wiki_search/par_bfs4.c
+++ wiki_search/par_bfs4.c
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <assert.h>
 
-#include "par_bfs3.h"
+#include "par_bfs4.h"
 #include "map_vec.h"
 #include "frontier.h"
 #include "explored_vec.h"
@@ -11,29 +11,29 @@
 #include "fr_pair.h"
 #include "linked_frnts.h"
 
-map_vec* global_map3;
-explored* global_dists3;
+map_vec* global_map4;
+explored* global_dists4;
 
-typedef struct job3 {
+typedef struct job4 {
     int thread_num;
     linked_frnts* lfrnt;
     long fr_start;
     long fr_end;
     int num_threads;
-} job3;
+} job4;
 
-void succs_to_fr3(frontier* fr, fr_pair* start_node) {
-    node* succ_nodes = map_get_node(global_map3, start_node->node_val);
+void succs_to_fr4(frontier* fr, fr_pair* start_node) {
+    node* succ_nodes = map_get_node(global_map4, start_node->node_val);
     for (long n = 0; n < succ_nodes->size; n++) {
         long succ = succ_nodes->data[n];
-        if (global_dists3->data[succ] == -1) {
+        if (global_dists4->data[succ] == -1) {
             fr_pair* new_frp = new_pair(succ, start_node->dist + 1);
             push_frontier(fr, (long)new_frp);
         }
    }
 }
 
-frontier* bfs_worker3(int t_num, long fr_start, long fr_end, linked_frnts* lfrnt, int num_threads) {
+frontier* bfs_worker4(int t_num, long fr_start, long fr_end, linked_frnts* lfrnt, int num_threads) {
     frontier* out_fr = make_frontier();
     long num_expanded = 0;
 
@@ -56,16 +56,16 @@ frontier* bfs_worker3(int t_num, long fr_start, long fr_end, linked_frnts* lfrnt
         fr_pair* cur_pair = (fr_pair*)get_item_frontier(cur_link->lfr, fr_index);
         num_expanded++;
 
-        if (global_dists3->data[cur_pair->node_val] != -1) {
+        if (global_dists4->data[cur_pair->node_val] != -1) {
             free(cur_pair);
             fr_index++;
             index++;
             continue;
         }
  
-        global_dists3->data[cur_pair->node_val] = cur_pair->dist;
+        global_dists4->data[cur_pair->node_val] = cur_pair->dist;
 
-        succs_to_fr3(out_fr, cur_pair);
+        succs_to_fr4(out_fr, cur_pair);
         free(cur_pair);
         
         fr_index++;
@@ -75,14 +75,14 @@ frontier* bfs_worker3(int t_num, long fr_start, long fr_end, linked_frnts* lfrnt
     return out_fr;
 }
 
-void* bfs_worker_start3(void* arg) {
-    job3 j = *((job3*)arg);
+void* bfs_worker_start4(void* arg) {
+    job4 j = *((job4*)arg);
     free(arg);
-    frontier* ret_fr = bfs_worker3(j.thread_num, j.fr_start, j.fr_end, j.lfrnt, j.num_threads);
+    frontier* ret_fr = bfs_worker4(j.thread_num, j.fr_start, j.fr_end, j.lfrnt, j.num_threads);
     return (void*)ret_fr;
 }
 
-void run_bfs_workers3(int num_threads, linked_frnts* start_lfrnt) {
+void run_bfs_workers4(int num_threads, linked_frnts* start_lfrnt) {
     linked_frnts* cur_lfrnt = start_lfrnt;
     while(1) {
         if (cur_lfrnt->tot_size <= 0) {
@@ -101,7 +101,7 @@ void run_bfs_workers3(int num_threads, linked_frnts* start_lfrnt) {
             } else {
                 end = (i + 1) * partition_size;
             }
-            job3* temp_job = malloc(sizeof(job3));
+            job4* temp_job = malloc(sizeof(job4));
             temp_job->thread_num = i;
             temp_job->lfrnt = cur_lfrnt;
             temp_job->fr_start = start;
@@ -113,7 +113,7 @@ void run_bfs_workers3(int num_threads, linked_frnts* start_lfrnt) {
             //printf("Frontier end:   %ld\n", end);
 
             pthread_t thread;
-            int rv = pthread_create(&thread, 0, bfs_worker_start3, temp_job);
+            int rv = pthread_create(&thread, 0, bfs_worker_start4, temp_job);
             assert(rv == 0);
             threads[i] = thread;
         }
@@ -132,28 +132,28 @@ void run_bfs_workers3(int num_threads, linked_frnts* start_lfrnt) {
     }    
 }
 
-int par_bfs3(map_vec* map, long source, int num_threads, int print_output) {
-    global_map3 = map;
+int par_bfs4(map_vec* map, long source, int num_threads, int print_output) {
+    global_map4 = map;
     linked_frnts* init_lfrnt = make_lfrnts();
     frontier* init_fr = make_frontier();
-    global_dists3 = make_explored();
-    for (long i = 0; i < global_map3->size; i++) {
-        push_explored(global_dists3, -1);
+    global_dists4 = make_explored();
+    for (long i = 0; i < global_map4->size; i++) {
+        push_explored(global_dists4, -1);
     }
     fr_pair* init_pair = new_pair(source, 0);
-    global_dists3->data[source] = 0;
-    succs_to_fr3(init_fr, init_pair);
+    global_dists4->data[source] = 0;
+    succs_to_fr4(init_fr, init_pair);
     add_frnt(init_lfrnt, init_fr);
 
-    run_bfs_workers3(num_threads, init_lfrnt);
+    run_bfs_workers4(num_threads, init_lfrnt);
     
     if (print_output) {
-        for (long i = 0; i < global_dists3->size; i++) {
-            printf("DIST FROM %ld TO %ld = %ld\n", source, i, global_dists3->data[i]);
+        for (long i = 0; i < global_dists4->size; i++) {
+            printf("DIST FROM %ld TO %ld = %ld\n", source, i, global_dists4->data[i]);
         }
     }
     free(init_pair);
-    free_explored(global_dists3);
+    free_explored(global_dists4);
 
     return 0;
 }
