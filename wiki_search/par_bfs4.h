67766350eae497627a052905eedac444a8e6ada4
Liam Douglass <liamadouglass@gmail.com>
2019-04-21 16:33:20 -0400

    modified the ifndef statement to be correct

--- wiki_search/par_bfs4.h
+++ wiki_search/par_bfs4.h
@@ -1,7 +1,7 @@
 #include "map_vec.h"
 
-#ifndef PAR_BFS3_H
-#define PAR_BFS3_H
+#ifndef PAR_BFS4_H
+#define PAR_BFS4_H
 
 int par_bfs4(map_vec* map, long source, int num_threads, int print_output);
 
