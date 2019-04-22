9723aeb11a021da3b4b943165fbf1fd053599cdd
Liam Douglass <liamadouglass@gmail.com>
2019-04-21 22:32:43 -0400

    changed benchmarking procedures

--- wiki_search/search.c
+++ wiki_search/search.c
@@ -159,12 +159,12 @@ int main(int argc, char* argv[]) {
         
         printf("Finished parsing.\n");
         printf("Elapsed parse time: %f\n\n", get_seconds(start, end));
-        /*         
+                 
         printf("Benchmarking Dijkstra...\n");
         double total = 0.0;
         for (int i = 0; i < 100; i++) {
             gettimeofday(&start, NULL);
-            dijkstra(map, 505, 0);;
+            dijkstra(map, 0, 0);;
             gettimeofday(&end, NULL);
             double secs = get_seconds(start, end);
             printf("%f\n", secs);
@@ -172,13 +172,13 @@ int main(int argc, char* argv[]) {
         }
         printf("Average time: %f secs\n", total / 100.0);
         printf("Dijkstra Done.\n");
-        */
-        for (int n = 1; n < 5; n++) {
+        
+        for (int n = 1; n < 4; n++) {
             printf("\nBenchmarking PBFS %d threads...\n", n);
             double total = 0.0;
             for (int i = 0; i < 100; i++) {
                 gettimeofday(&start, NULL);
-                par_bfs4(map, 505, n, 0);
+                par_bfs4(map, 0, n, 0);
                 gettimeofday(&end, NULL);
                 double secs = get_seconds(start, end);
                 printf("%f\n", secs);
